a8e2460d221f298981efab3e248b9c0049c1b0be
Mdowel <150044412+Mdowel@users.noreply.github.com>
2024-12-26 12:20:00 -0500

    basic html setup

---
+++ config.js
@@ -0,0 +1,13 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                dark_teal: '#1C4249',
+                light_teal: '#8AB8A2',
+                cream: '#FAFFDB',
+                pesto: '#C7AF3F',
+                my_black: '#292D30'
+            }
+        }
+    }
+}
